3fdf91d67e1b326f595f594e718737f3ef0aead6
tianyu5177 <zhangtianyu5177@sina.com>
2020-10-09 15:23:49 +0800

    更新

---
+++ 07_src_github_pubsub/index.js
@@ -0,0 +1,8 @@
+//引入react核心库
+import React from 'react'
+//1.引入App
+import App from './App'
+//2.引入ReactDOM
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<App/>,document.getElementById('root'))
